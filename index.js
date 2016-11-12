8a4dc71498f126361e29ea1cc656edc02fa90425
Aaron Fox <Aaron Fox>
2016-11-12 13:44:02 -0500

    updated the bot to speak

--- index.js
+++ index.js
@@ -1,53 +1,66 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000))
 
-// Server frontpage
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
 app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
+    res.send('Hello world, I am a chat bot')
+})
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'am_i_verified') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
     }
-});
+    res.send('Error, wrong token')
+})
 
-// handler receiving messages
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
     }
-    res.sendStatus(200);
-});
+    res.sendStatus(200)
+})
 
-/ generic function sending messages
-function sendMessage(recipientId, message) {
+var token = "EAACH17k9j0oBALuERxI0mphZCXdkJ8mM6VP1mwCws8IdkpcltA09FPYob1bdWxyND9Y4X56uoNRJ8A4ehoS68jP5ReOlwYG50aeMZAYUkAHWpOXbowZCbK8ueRZBmS3C1l6vQfnAlnu03lIKWqZCFs1bOIAvoKnglZB5DPMMW5AQZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        qs: {access_token:token},
         method: 'POST',
         json: {
-            recipient: {id: recipientId},
-            message: message,
+            recipient: {id:sender},
+            message: messageData,
         }
     }, function(error, response, body) {
         if (error) {
-            console.log('Error sending message: ', error);
+            console.log('Error sending messages: ', error)
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+            console.log('Error: ', response.body.error)
         }
-    });
-};
+    })
+}
