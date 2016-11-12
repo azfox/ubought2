5938d3d84b7f84876251d35b757fd9f6c1ea3c56
Aaron Fox <Aaron Fox>
2016-11-12 16:20:12 -0500

    Probably Broken Code

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ var token = "EAACH17k9j0oBALuERxI0mphZCXdkJ8mM6VP1mwCws8IdkpcltA09FPYob1bdWxyND9
 var verify = "am_i_verified"
 var os = require('os');
 var BotKit = require('botkit');
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
+app.set('port', (process.env.PORT || 5000))
 
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'am_i_verified') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 
 var controller = BotKit.facebookbot({
     debug: false,
