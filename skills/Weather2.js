fd49a85b6e6dede5e2b93e705482b8622b8ddf4a
Aaron Fox <Aaron Fox>
2016-11-17 23:29:32 -0500

    more going on

--- skills/Weather2.js
+++ skills/Weather2.js
@@ -62,7 +62,7 @@ try {
   location = message.intents.entities.location.value
 }
 catch(err) {
-  console.log(message.intents.entities)
+  console.log(message.intents)
   //console.log(message.intents.entities[0])
   bot.reply(message, "Oh Man.  I would love to tell you the Weather but you never told me where...the world is vastly different you know!!")
   return;
