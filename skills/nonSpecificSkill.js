0f9f73f1fba7c39d556b43f5c81c7e06f399a73a
Aaron Fox <Aaron Fox>
2016-11-23 14:05:47 -0500

    adding bing boi

--- skills/nonSpecificSkill.js
+++ skills/nonSpecificSkill.js
@@ -6,34 +6,42 @@ module.exports = function(skill, info, bot, message) {
 var Bing = require('node-bing-api')({ accKey: process.env.BING_ACCT_KEY });
 
 var query = ''
-if(message.intents[0].entities["search_query"][0].value && message.intents[0].entities["search_query"][0].value != null
-    && message.intents[0].entities["search_query"][0].value != ''){
-  query = message.intents[0].entities["search_query"][0].value
-}else{
-  bot.reply(message, '...Hmm... I don\'t have a response what you said... I\'ll save it and try to learn about it later. What else can i do for you??'); //LOL no i wont
-  return;
+
+//error catch all right now cuz i am lazy and don't want to learn JSON ...
+try{
+  if(message.intents[0].entities["search_query"][0].value && message.intents[0].entities["search_query"][0].value != null
+      && message.intents[0].entities["search_query"][0].value != ''){
+    query = message.intents[0].entities["search_query"][0].value
+  }else{
+    bot.reply(message, '...Hmm... I don\'t have a response what you said... I\'ll save it and try to learn about it later. What else can i do for you??'); //LOL no i wont
+    return;
+  }
+  if(1 == 2){
+    //here is where i will handle wit ai stories and shit what the qery
+    //but on any error in the query, i will just search for it on bing
+  }else{
+    Bing.web(query, {
+        top: 10,  // Number of results (max 50)
+        skip: 3   // Skip first 3 results that i assume to be Adds... take that MSFT!!!!
+      }, function(error, res, body){
+
+        if(error){
+          bot.reply(message, "I tried a web search for: " + query + "\n BUT I may have run run out of Free API calls to Bing search..."
+                            + " You know you got a second Rate bot when he/she/it/(GOD??) is trying to use Bing instead of the GOOGLE");
+          return;
+        }
+
+        send4BingResults(bot,message,query,body);
+
+        //console.log(body.webPages.value[0]);
+        //console.log(body.webPages.value[1]);
+      });
+  }
+
+} catch(err){
+  bot.reply(message, "You broke me, I don't quite not what to say")
 }
-if(1 == 2){
-  //here is where i will handle wit ai stories and shit what the qery
-  //but on any error in the query, i will just search for it on bing
-}else{
-  Bing.web(query, {
-      top: 10,  // Number of results (max 50)
-      skip: 3   // Skip first 3 results that i assume to be Adds... take that MSFT!!!!
-    }, function(error, res, body){
 
-      if(error){
-        bot.reply(message, "I tried a web search for: " + query + "\n BUT I may have run run out of Free API calls to Bing search..."
-                          + " You know you got a second Rate bot when he/she/it/(GOD??) is trying to use Bing instead of the GOOGLE");
-        return;
-      }
-
-      send4BingResults(bot,message,query,body);
-
-      //console.log(body.webPages.value[0]);
-      //console.log(body.webPages.value[1]);
-    });
-}
 
 
 };
