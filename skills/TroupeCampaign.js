aef054727947bb10e4af16314f8f33ddc0bea7f7
Aaron Fox <Aaron Fox>
2016-11-24 09:50:11 -0500

    troupe campaign

--- skills/TroupeCampaign.js
+++ skills/TroupeCampaign.js
@@ -1,6 +1,6 @@
 module.exports = function(skill, info, bot, message) {
 
-console.log(message.type)
+console.log(message)
 bot.reply("yooooooooooo dude check it out! My illest new creation from #TroupeJewelry...You DO NOT want to miss this")
 
 
