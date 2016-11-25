2e7a9ee063e42aa5936a83fe21dca948861b769b
Aaron Fox <Aaron Fox>
2016-11-24 19:40:11 -0500

    troupe campaign

--- skills/TroupeCampaign.js
+++ skills/TroupeCampaign.js
@@ -83,7 +83,7 @@ function sendTroupeVideo(bot,message,img_url, campaign_url, uTube) {
     attachments: attachment,
   })
 
-  sendTroupeCharm2(bot,message, img_url, campaign_url);
+  //sendTroupeCharm2(bot,message, img_url, campaign_url);
 
 
 }
