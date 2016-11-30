283abb1714a2c729b6b4b1761413dccf3c4f9eab
Aaron Fox <Aaron Fox>
2016-11-29 22:32:29 -0500

    mortage attempt 2

--- skills/Mortgage.js
+++ skills/Mortgage.js
@@ -15,37 +15,66 @@ module.exports = function(skill, info, bot, message) {
       // create a path where neither option was matched
       // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
       convo.addMessage({
-          text: 'Sorry I did not understand.',
+          text: 'Sorry I did not understand. Try again',
           action: 'default',
       },'bad_response');
 
-      // Create a yes/no question in the default thread...
-      convo.ask('So you are looking for a new Mortage huh?', [
-          {
-              pattern: 'yes',
-              callback: function(response, convo) {
-                  convo.changeTopic('yes_thread');
-              },
-          },
-          {
-              pattern: 'no',
-              callback: function(response, convo) {
-                  convo.changeTopic('no_thread');
-              },
-          },
-          {
-              default: true,
-              callback: function(response, convo) {
-                  convo.changeTopic('bad_response');
-              },
-          }
-      ]);
+      init_question("So, you are looking for a new Mortage, huh?")
 
       convo.activate();
   });
 
 
 };
+
+
+function init_question(question, convo){
+    var vid = {
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":question,
+        "buttons":[
+          {
+            "type":"postback",
+            "title":"Yessir",
+            "payload": "yes"
+          },
+          {
+            "type":"postback",
+            "title":"No Thanks",
+            "payload": "no"
+          }
+        ]
+      }
+    }
+    // Create a yes/no question in the default thread...
+
+    convo.ask('So you are looking for a new Mortage huh?', [
+        {
+            pattern: 'yes',
+            callback: function(response, convo) {
+                convo.changeTopic('yes_thread');
+            },
+        },
+        {
+            pattern: 'no',
+            callback: function(response, convo) {
+                convo.changeTopic('no_thread');
+            },
+        },
+        {
+            default: true,
+            callback: function(response, convo) {
+                convo.changeTopic('bad_response');
+            },
+        }
+    ]);
+
+  }
+
+
+
 
 
 
