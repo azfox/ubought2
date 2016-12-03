dd5d34f5811f9d8934c880fdd016a2847596383c
Aaron Fox <Aaron Fox>
2016-12-03 09:47:52 -0500

    adding threads

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ console.log('Finished Express..requiring App')
 
 // routes
 require('./app/routes/routes')(app)
-//require('./skills/Mortgage')
-require('./helpers/MortageHelpers');
+require('./skills/Mortgage')
+//require('./helpers/MortageHelpers');
 
 // port for Heroku
 app.set('port', (process.env.PORT || 5000))
