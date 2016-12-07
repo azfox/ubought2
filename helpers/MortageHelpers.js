1db14e3ca2c929e52b76b5aaea26d6c2a82c5f47
Aaron Fox <Aaron Fox>
2016-12-06 19:33:45 -0500

    blah

--- helpers/MortageHelpers.js
+++ helpers/MortageHelpers.js
@@ -36,7 +36,8 @@ exports.get_name_from_uid = function(uid){
        }
        else {
          //console.log(body)
-         //console.log(response.body.first_name)
+         console.log(response.body.first_name)
+         console.log(response.body.last_name)
          return response.body.first_name + " " + response.body.last_name
 
        }
